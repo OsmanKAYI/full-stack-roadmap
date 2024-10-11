dabcf4e9fcc4f5543dd30c34a4f44114a8d98a1d
Osman KAYI <osmankayi06@gmail.com>
2024-10-11 14:10:45 +0300

    mendeley reference manager added to fav apps

--- linux/setup.necessary.features.on.linux.24.04.sh
+++ linux/setup.necessary.features.on.linux.24.04.sh
@@ -444,7 +444,7 @@ echo -e '[Desktop Entry]\nName=TUXEDO Control Center\nComment=Tray icon for TUXE
 echo -e '[Desktop Entry]\nName=Ulauncher\nComment=Application launcher for Linux\nExec=env GDK_BACKEND=x11 /usr/bin/ulauncher --hide-window --hide-window\nIcon=ulauncher\nTerminal=false\nType=Application\nCategories=Utility;Application;' > ~/.config/autostart/ulauncher.desktop
 
 # Favorite Applications
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.SystemMonitor.desktop', 'virtualbox.desktop', 'thunderbird.desktop', 'firefox.desktop', 'chromium-browser.desktop', 'telegram.desktop', 'Postman.desktop', 'code.desktop', 'wps-office-prometheus.desktop', 'org.fritzing.Fritzing.desktop']"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'org.gnome.SystemMonitor.desktop', 'virtualbox.desktop', 'thunderbird.desktop', 'firefox.desktop', 'chromium-browser.desktop', 'telegram.desktop', 'Postman.desktop', 'code.desktop', 'wps-office-prometheus.desktop', 'com.elsevier.MendeleyDesktop.desktop', 'org.fritzing.Fritzing.desktop']"
 
 # Settings
 ## appearance
